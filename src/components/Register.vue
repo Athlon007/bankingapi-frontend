53f7947d0f86ff393313085f246b94a4e94bdb26
Athlon007 <aathlon@outlook.com>
2023-06-02 15:46:07 +0200

    ff

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,11 +1,64 @@
 <template>
-    <div>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-6 form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" class="d-input w-100" v-model="this.username"
+                        v-on:keyup-enter="register" />
+                </div>
+                <div class="col-6 form-group">
+                    <label for="username">E-Mail</label>
+                    <input type="email" id="username" name="username" class="d-input w-100" v-model="this.username"
+                        v-on:keyup-enter="register" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" class="d-input w-100" v-model="this.password"
+                        v-on:keyup-enter="register" />
+                </div>
+                <div class="col-6 form-group">
+                    <label for="password">Confirm Password</label>
+                    <input type="password" id="password" name="password" class="d-input w-100" v-model="this.password"
+                        v-on:keyup-enter="register" />
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-6 form-group">
+                    <label for="firstname">First Name</label>
+                    <input type="text" id="firstname" name="firstname" class="d-input w-100" v-model="this.firstname"
+                        v-on:keyup-enter="register" />
+                </div>
+                <div class="col-6 form-group">
+                    <label for="firstname">Last Name</label>
+                    <input type="text" id="firstname" name="firstname" class="d-input w-100" v-model="this.firstname"
+                        v-on:keyup-enter="register" />
+                </div>
+            </div>
+            <div class="row justify-content-center">
 
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+import { useUserSessionStore } from "../stores/usersession.js";
+
 export default {
+    name: "Register",
+    data() {
+        return {
+            users: []
+        };
+    },
+    mounted() {
+        if (useUserSessionStore().isAuthenticated) {
+            this.$router.push("/dashboard");
+        }
+    }
 
 }
 </script>
