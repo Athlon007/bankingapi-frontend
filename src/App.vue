ba4bee934bdd00f22d088404848bc756fa8bc331
Athlon007 <aathlon@outlook.com>
2023-06-02 22:50:58 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,6 @@ export default {
   <RouterView />
   <div class="container">
     <footer class="py-3 my-4">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-      </ul>
       <p class="text-center text-body-secondary">© 2023 IT2A Group I actually forgot</p>
       <p class="text-center text-body-secondary">This website is not actually InHolland's website, instead it is an
         university project.</p>
