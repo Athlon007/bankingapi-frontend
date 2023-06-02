68b620fe4a73d8d4cca3e5cca5beaefbbc9e747a
Athlon007 <aathlon@outlook.com>
2023-06-02 22:49:57 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,22 @@ export default {
 <template>
   <navigation />
   <RouterView />
+  <div class="container">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+      </ul>
+      <p class="text-center text-body-secondary">© 2023 IT2A Group I actually forgot</p>
+      <p class="text-center text-body-secondary">This website is not actually InHolland's website, instead it is an
+        university project.</p>
+      <p class="text-center text-body-secondary">If something doesn't work, <a
+          href="https://www.youtube.com/watch?v=nqxVMLVe62U" target="_blank">Blame It on the Boogie.</a></p>
+    </footer>
+  </div>
 </template>
 
 <style>
