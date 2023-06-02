815409bdcf67c93cb704c1b066f4e05ee703f205
Athlon007 <aathlon@outlook.com>
2023-06-02 22:37:19 +0200

    User Management done

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -8,6 +8,11 @@ import useEmitter from '../emitter';
 <template>
     <div class="container">
         <h2>User Management</h2>
+        <div class="row" v-if="this.error">
+            <div class="alert alert-danger">
+                {{ this.error }}
+            </div>
+        </div>
         <div class="row">
             <div class="col-6">
                 <form>
@@ -37,65 +42,146 @@ import useEmitter from '../emitter';
                     <UserManagementListItem v-for="user in users" :key="user.id" :user="user" />
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-6" :class="edited_user == null ? 'disabled' : ''">
                 <div class="row">
-                    <div class="col-6 form-group">
-                        <label for="username">Username</label>
-                        <input type="text" id="username" name="username" class="d-input w-100"
-                            v-model="this.edited_user.username" @keyup.enter="register" />
+                    <div class="col-12">
+                        <button type="button" class="btn btn-primary mx-2" @click="save"
+                            v-if="this.edited_user.active">Save</button>
+                        <button type="button" class="btn btn-success mx-2" @click="save" v-else>Activate</button>
+                        <button type="button" class="btn btn-light mx-2" @click="cancel">Cancel</button>
+                        <button type="button" class="btn btn-danger mx-2" @click="this.delete()">{{
+                            this.edited_user.current_account == null ? "Delete" : "Deactivate" }}</button>
                     </div>
-                    <div class="col-6 form-group">
-                        <label for="email">E-Mail</label>
-                        <input type="email" id="email" name="email" class="d-input w-100" v-model="this.edited_user.email"
-                            @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" class="d-input w-100"
+                        v-model="this.edited_user.username" @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="email">E-Mail</label>
+                    <input type="email" id="email" name="email" class="d-input w-100" v-model="this.edited_user.email"
+                        @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" class="d-input w-100"
+                        v-model="this.edited_user.password" @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="password">Confirm Password</label>
+                    <input type="password" id="password" name="password" class="d-input w-100"
+                        v-model="this.password_confirm" @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="firstname">First Name</label>
+                    <input type="text" id="firstname" name="firstname" class="d-input w-100"
+                        v-model="this.edited_user.firstname" @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="lastname">Last Name</label>
+                    <input type="text" id="lastname" name="lastname" class="d-input w-100"
+                        v-model="this.edited_user.lastname" @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="bsn">BSN</label>
+                    <input type="number" id="bsn" name="bsn" class="d-input w-100" v-model="this.edited_user.bsn"
+                        @keyup.enter="register" @keypress="isValidBsn($event)" />
+                </div>
+                <div class="form-group">
+                    <label for="phone_number">Phone Number</label>
+                    <input type="tel" id="phone_number" name="phone_number" class="d-input w-100"
+                        v-model="this.edited_user.phone_number" @keyup.enter="register" />
+                </div>
+                <div class="form-group">
+                    <label for="birth_date">Birth Date</label>
+                    <input type="date" id="birth_date" name="birth_date" class="d-input w-100"
+                        v-model="this.edited_user.birth_date" @keyup.enter="register" />
+                </div>
+                <div class="form-group" v-if="this.isCurrentUserAdmin">
+                    <label for="role">Role</label>
+                    <select id="role" name="role" class="d-input w-100" v-model="this.edited_user.role"
+                        @keyup.enter="register">
+                        <option value="ADMIN">Admin</option>
+                        <option value="EMPLOYEE">Employee</option>
+                        <option value="USER">User</option>
+                    </select>
+                </div>
+                <div class="card my-2 p-2">
+                    <div class="row">
+                        <h3>Current Account</h3>
+                    </div>
+                    <div class="row" v-if="this.edited_user.current_account == null">
+                        <p>No Account</p>
+                        <button type="button" class="btn btn-primary" @click="createAccount('current')">Create
+                            Account</button>
+                    </div>
+                    <div class="row" v-else>
+                        <p><strong>Account ID:</strong> {{ this.edited_user.current_account.id }}</p>
+                        <p><strong>IBAN:</strong> {{ this.edited_user.current_account.IBAN }}</p>
+                        <p><strong>Balance:</strong> {{ this.edited_user.current_account.balance }}</p>
+                        <p><strong>Active: </strong> {{ this.edited_user.current_account.isActive ? "Yes" : "No" }}</p>
+                        <div class="col-12">
+                            <button type="button" class="btn btn-danger" @click="deactivateAccount('current')"
+                                v-if="this.edited_user.current_account.isActive">Deactivate Account</button>
+                            <button type="button" class="btn btn-success" @click="activateAccount('current')"
+                                v-else>Activate
+                                Account</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card my-2 p-2" v-if="this.edited_user.current_account != null">
+                    <div class="row">
+                        <h3>Saving Account</h3>
+                    </div>
+                    <div class="row" v-if="this.edited_user.saving_account == null">
+                        <p>No Account</p>
+                        <button type="button" class="btn btn-primary" @click="createAccount('saving')">Create
+                            Account</button>
+                    </div>
+                    <div class="row" v-else>
+                        <p><strong>Account ID:</strong> {{ this.edited_user.saving_account.id }}</p>
+                        <p><strong>IBAN:</strong> {{ this.edited_user.saving_account.IBAN }}</p>
+                        <p><strong>Balance:</strong> {{ this.edited_user.saving_account.balance }}</p>
+                        <p><strong>Active: </strong> {{ this.edited_user.saving_account.isActive ? "Yes" : "No" }}</p>
+                        <div class="col-12">
+                            <button type="button" class="btn btn-danger" @click="deactivateAccount('saving')"
+                                v-if="this.edited_user.saving_account.isActive">Deactivate Account</button>
+                            <button type="button" class="btn btn-success" @click="editAccount" v-else>Activate
+                                Account</button>
+                        </div>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-6 form-group">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" class="d-input w-100"
-                            v-model="this.edited_user.password" @keyup.enter="register" />
-                    </div>
-                    <div class="col-6 form-group">
-                        <label for="password">Confirm Password</label>
-                        <input type="password" id="password" name="password" class="d-input w-100"
-                            v-model="this.password_confirm" @keyup.enter="register" />
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-6 form-group">
-                        <label for="firstname">First Name</label>
-                        <input type="text" id="firstname" name="firstname" class="d-input w-100"
-                            v-model="this.edited_user.firstname" @keyup.enter="register" />
-                    </div>
-                    <div class="col-6 form-group">
-                        <label for="lastname">Last Name</label>
-                        <input type="text" id="lastname" name="lastname" class="d-input w-100"
-                            v-model="this.edited_user.lastname" @keyup.enter="register" />
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-6 form-group">
-                        <label for="bsn">BSN</label>
-                        <input type="number" id="bsn" name="bsn" class="d-input w-100" v-model="this.edited_user.bsn"
-                            @keyup.enter="register" @keypress="isValidBsn($event)" />
-                    </div>
-                    <div class="col-6 form-group">
-                        <label for="phone_number">Phone Number</label>
-                        <input type="tel" id="phone_number" name="phone_number" class="d-input w-100"
-                            v-model="this.edited_user.phone_number" @keyup.enter="register" />
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-6 form-group">
-                        <label for="birth_date">Birth Date</label>
-                        <input type="date" id="birth_date" name="birth_date" class="d-input w-100"
-                            v-model="this.edited_user.birth_date" @keyup.enter="register" />
+                    <div class="col-12">
+                        <button type="button" class="btn btn-primary mx-2" @click="save"
+                            v-if="this.edited_user.active">Save</button>
+                        <button type="button" class="btn btn-success mx-2" @click="save" v-else>Activate</button>
+                        <button type="button" class="btn btn-light mx-2" @click="cancel">Cancel</button>
+                        <button type="button" class="btn btn-danger mx-2" @click="this.delete()">{{
+                            this.edited_user.current_account == null ? "Delete" : "Deactivate" }}</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <dialog id="delete-account-dialog">
+        <div class="row">
+            <h2>Warning</h2>
+        </div>
+        <hr />
+        <div class="row">
+            <p>Are you sure you want to <strong>{{ hasNoAccount ? "remove" : "deactivate" }}</strong> this account?</p>
+            <strong>
+                <p>{{ this.edited_user.firstname }} {{ this.edited_user.lastname }}</p>
+            </strong>
+            <p v-if="!hasNoAccount">This action can only be reverted by contacting customer support.</p>
+        </div>
+        <div class="row d-flex justify-content-evenly">
+            <button class="btn btn-danger w-25" @click="deleteAccount()">Yes</button>
+            <button class="btn btn-primary w-25" @click="closeDialog">No</button>
+        </div>
+    </dialog>
 </template>
 
 <script>
@@ -114,7 +200,9 @@ export default {
             limit: 20,
             isMorePages: true,
             lastQuery: {},
-            edited_user: {}
+            edited_user: {},
+            isCurrentUserAdmin: false,
+            password_confirm: ""
         }
     },
     methods: {
@@ -153,8 +241,7 @@ export default {
 
                 })
                 .catch(error => {
-                    console.log(error);
-                    this.error = error;
+                    this.error = error.response.data.error_message;
                 });
         },
         nextPage() {
@@ -164,14 +251,88 @@ export default {
         searchOnSearchbar(event) {
             event.preventDefault();
             this.search();
+        },
+        createAccount(account_type) {
+
+            const request = {
+                accountType: account_type.toUpperCase(),
+                IBAN: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+                userId: this.edited_user.id,
+                currencyType: "EURO"
+            }
+
+            axios.post(`/accounts`, request)
+                .then(response => {
+                    if (account_type === "current")
+                        this.edited_user.current_account = response.data;
+                    else
+                        this.edited_user.saving_account = response.data;
+                })
+                .catch(error => {
+                    this.error = error.response.data.error_message;
+                });
+        },
+        deactivateAccount(account_type) { },
+        save() {
+            this.error = "";
+
+            const request = {
+                username: this.edited_user.username,
+                email: this.edited_user.email,
+                firstname: this.edited_user.firstname,
+                lastname: this.edited_user.lastname,
+                bsn: this.edited_user.bsn,
+                phone_number: this.edited_user.phone_number,
+                birth_date: this.edited_user.birth_date
+            }
+
+            // If this user is admin, add role too.
+            if (this.isCurrentUserAdmin) {
+                request.role = this.edited_user.role;
+            }
+
+            axios.put(`/users/${this.edited_user.id}`, request)
+                .then(response => {
+                    useEmitter().emit("user-edit-save");
+                })
+                .catch(error => {
+                    this.error = error.response.data.error_message;
+                });
+
+        },
+        cancel() {
+            useEmitter().emit("user-edit-end");
+        },
+        delete() {
+            document.getElementById("delete-account-dialog").showModal();
+        },
+        deleteAccount() {
+            axios.delete(`/users/${this.edited_user.id}`)
+                .then(response => {
+                    this.closeDialog();
+                    useEmitter().emit("user-edit-save");
+                })
+                .catch(error => {
+                    this.error = error.response.data.error_message;
+                });
+        },
+        closeDialog() {
+            document.getElementById("delete-account-dialog").close();
         }
     },
-    mounted() {
-        useUserSessionStore().localLogin();
+    async mounted() {
+        await useUserSessionStore().localLogin();
+
+        // we don't want regular users to access this page
+        if (useUserSessionStore().user == null || useUserSessionStore().user.role !== "ADMIN" && useUserSessionStore().user.role !== "EMPLOYEE") {
+            this.$router.push("/");
+        }
+
         this.search();
+        this.isCurrentUserAdmin = useUserSessionStore().user.role === "ADMIN";
 
         useEmitter().on("activate-user", (user) => {
-            this.edited_user = user;
+            this.edited_user = JSON.parse(JSON.stringify(user));
         });
 
         useEmitter().on("user-edit-end", () => {
@@ -180,6 +341,7 @@ export default {
 
         useEmitter().on("user-edit-save", () => {
             this.doQuery(this.lastQuery);
+            useEmitter().emit("user-edit-end");
         });
     }
 }
@@ -199,5 +361,14 @@ export default {
 
 .no-border-right {
     border-right: none;
+}
+
+.disabled {
+    pointer-events: none;
+    cursor: default;
+}
+
+.container {
+    margin-bottom: 2rem;
 }
 </style>
