5787b8662ea844b8361717d518cbfbc2ab98ef7f
robytrierweiler <roby.trierweiler@energie-pass.lu>
2023-06-07 01:39:25 +0200

    http

--- src/axios_auth.js
+++ src/axios_auth.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
+<<<<<<< Updated upstream
     baseURL: 'https://bankapi-inholland-project.onrender.com/'
     //baseURL: 'http://localhost:8443/'
+=======
+    baseURL: 'http://localhost:8443/'
+>>>>>>> Stashed changes
 });
 
 instance.defaults.headers.post['Content-Type'] = 'application/json';
