66f7f489032d58b57ffda33445f1aaee3ae69d79
Athlon007 <aathlon@outlook.com>
2023-06-07 09:56:48 +0200

    Add icons to nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -9,44 +9,48 @@
         <router-link to="/" class="navbar-brand fw-bolder">InHolland Bank</router-link>
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <router-link to="/dashboard" class="nav-link" active-class="active"
-              v-if="this.user != null">Dashboard</router-link>
+            <router-link to="/dashboard" class="nav-link" active-class="active" v-if="this.user != null"><i
+                class="bi bi-house-door-fill"></i>Dashboard</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/transfer" class="nav-link" active-class="active"
-              v-if="this.user != null">Transfer</router-link>
+            <router-link to="/transfer" class="nav-link" active-class="active" v-if="this.user != null"><i
+                class="bi bi-send-fill"></i>Transfer</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/atm" class="nav-link" active-class="active"
-              v-if="this.user != null">ATM</router-link>
+            <router-link to="/atm" class="nav-link" active-class="active" v-if="this.user != null"><i
+                class="bi bi-credit-card-fill"></i>ATM</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/transactionhistory" class="nav-link" active-class="active" v-if="this.user != null">Transaction History
-              </router-link>
+            <router-link to="/transactionhistory" class="nav-link" active-class="active" v-if="this.user != null"><i
+                class="bi bi-clock-fill"></i>Transaction History
+            </router-link>
           </li>
           <li class="nav-item">
             <router-link to="/usermanagement" class="nav-link" active-class="active"
-              v-if="this.user != null && isEmployeeOrAdmin">User
+              v-if="this.user != null && isEmployeeOrAdmin"><i class="bi bi-kanban-fill"></i>User
               Management</router-link>
           </li>
         </ul>
         <ul class="navbar-nav">
           <div class="d-inline" v-if="this.user != null">
             <li class="nav-item d-inline">
-              <router-link to="/settings" class="nav-link d-inline" active-class="active">My Account ({{
-                this.user.firstname
-              }} {{ this.user.lastname }})</router-link>
+              <router-link to="/settings" class="nav-link d-inline" active-class="active"><i
+                  class="bi bi-person-fill"></i>My Account ({{
+                    this.user.firstname
+                  }} {{ this.user.lastname }})</router-link>
             </li>
             <li class="nav-item d-inline">
-              <a class="nav-link d-inline" @click="logoutClick">Logout</a>
+              <a class="nav-link d-inline" @click="logoutClick"><i class="bi bi-door-closed-fill"></i>Logout</a>
             </li>
           </div>
           <div class="d-inline" v-else>
             <li class="nav-item d-inline">
-              <router-link to="/register" class="nav-link d-inline" active-class="active">Register</router-link>
+              <router-link to="/register" class="nav-link d-inline" active-class="active"><i
+                  class="bi bi-person-fill-add"></i>Register</router-link>
             </li>
             <li class="nav-item d-inline">
-              <router-link to="/login" class="nav-link d-inline" active-class="active">Login</router-link>
+              <router-link to="/login" class="nav-link d-inline" active-class="active"><i
+                  class="bi bi-door-open-fill"></i>Login</router-link>
             </li>
           </div>
         </ul>
@@ -108,4 +112,7 @@ export default {
   cursor: pointer;
 }
 
+.bi {
+  margin-right: 0.3rem;
+}
 </style>
